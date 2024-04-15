b50ced7fa79c10880f6851f267128231bbc7eb74
worthant <b_dvorkin@niuitmo.ru>
2024-04-15 15:50:59 +0300

    :rocket: feat(1-var): New function for testing && log info

--- 1-var/src/main.rs
+++ 1-var/src/main.rs
@@ -2,8 +2,8 @@ mod methods;
 
 use methods::{Optimizer, Function};
 
-struct MyFunction;
-impl Function for MyFunction {
+struct Function1;
+impl Function for Function1 {
     fn f(&self, x: f64) -> f64 {
         0.5 * x.powi(2) - x.sin()
     }
@@ -17,9 +17,29 @@ impl Function for MyFunction {
     }
 }
 
+struct Function2;
+impl Function for Function2 {
+    fn f(&self, x: f64) -> f64 {
+        x.powi(3) + x.powi(2) - 7.0 * x
+    }
+
+    fn f_deriv(&self, x: f64) -> f64 {
+        3.0*x.powi(2) + 2.0*x - 7.0
+    }
+
+    fn f_deriv2(&self, x: f64) -> f64 {
+        6.0*x + 2.0
+    }
+}
+
 fn main() {
-    let func = MyFunction;
-    let opt = Optimizer::new(0.0, 1.0, 0.0001, &func);
+    let func = Function1;
+    let test1 = Optimizer::new(0.0, 1.0, 0.0001, &func);
+
+    let func2 = Function2;
+    let test2 = Optimizer::new(0.0, 2.0, 0.1, &func2);
+
+    let opt = test2;
     println!("Метод половинного деления: {}", opt.bisection());
     println!("Метод золотого сечения: {}", opt.golden_section());
     println!("Метод хорд: {}", opt.chord());
--- 1-var/src/methods/golden_section.rs
+++ 1-var/src/methods/golden_section.rs
@@ -5,15 +5,19 @@ pub fn golden_section(mut a: f64, mut b: f64, eps: f64, func: &dyn Function) -> 
     let mut x1 = a + (2.0 - phi) * (b - a);
     let mut x2 = a + (phi - 1.0) * (b - a);
 
+    println!("x1: {}, x2: {}", x1, x2);
     while (b - a) > 2.0 * eps {
+        println!("f(x1): {}, f(x2): {}", func.f(x1), func.f(x2));
         if func.f(x1) < func.f(x2) {
             b = x2;
             x2 = x1;
             x1 = a + (2.0 - phi) * (b - a);
+            println!("new x1: {}", x1);
         } else {
             a = x1;
             x1 = x2;
             x2 = a + (phi - 1.0) * (b - a);
+            println!("new x2: {}", x2);
         }
     }
     (a + b) / 2.0
