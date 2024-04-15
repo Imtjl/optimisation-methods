b3adc31227c4f6a2111ad8acc05d1cd11aa1fbae
worthant <b_dvorkin@niuitmo.ru>
2024-04-15 15:52:40 +0300

    :recycle: fix(lab1): Delete old unused files

--- lab1/src/methods/newton.rs
+++
@@ -1,9 +0,0 @@
-use super::Function;
-
-pub fn newton(a: f64, b: f64, eps: f64, func: &dyn Function) -> f64 {
-    let mut x = (a + b) / 2.0;
-    while func.f_deriv(x).abs() > eps {
-        x = x - func.f_deriv(x) / func.f_deriv2(x);
-    }
-    x
-}
